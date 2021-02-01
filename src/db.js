4dd93d9d3414f3c62b2d245c8d2bc7fd643b4ae2
atroncoso2 <atroncoso2@uc.cl>
2021-02-01 01:46:36 -0300

    db(db): seperate development from prodoction

--- src/db.js
+++ src/db.js
@@ -1,22 +1,35 @@
 const { Pool } = require('pg')
 require('dotenv').config()
 
-exports.start = async function() {
-    // const user = process.env.DB_USER
-    // const host = process.env.DB_HOST
-    // const database = process.env.DB_NAME
-    // const password = process.env.DB_PSWD
-    // const port = process.env.DB_PORT
+const node_env = process.env.NODE_ENV
 
-    // this.pool = new Pool({user, host, database, password, port})
-    // return {user, host, database, password, port}
+exports.start = async function() {
+
+    let pool_params
+
+    if (node_env === 'development') {
+        const user = process.env.DB_USER
+        const host = process.env.DB_HOST
+        const database = process.env.DB_NAME
+        const password = process.env.DB_PSWD
+        const port = process.env.DB_PORT
+
+        console.log(`db => ${database}:${port}`)
+
+        pool_params = {user, host, database, password, port}
+
+    } else {
+        const db_url = process.env.DATABASE_URL
+        console.log(`db => ${db_url}`)
+        pool_params = {
+            connectionString: db_url,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        }        
+    }
     
-    this.pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    })
+    this.pool = new Pool(pool_params)
 }
 
 exports.close = async function() {
--- src/index.js
+++ src/index.js
@@ -13,15 +13,6 @@ app.use(router.routes())
 exports.start = async function() {
     try {
         db_info = await db.start()
-        // console.log(
-        //     `db connected at 
-        //     port: ${db_info.port}
-        //     user: ${db_info.user}
-        //     database: ${db_info.database}
-        //     host: ${db_info.host}
-        //     password: ${db_info.password}
-        //     `
-        // )
         this.server = await app.listen(PORT)
         console.log(`Listening to PORT ${PORT}`)
     } catch (error) {
