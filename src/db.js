f7483b300df83742023b6cd0a7ed96c911b8734c
atroncoso2 <atroncoso2@uc.cl>
2021-02-01 01:37:00 -0300

    db url(db): edit db for deploy

--- src/db.js
+++ src/db.js
@@ -2,14 +2,21 @@ const { Pool } = require('pg')
 require('dotenv').config()
 
 exports.start = async function() {
-    const user = process.env.DB_USER
-    const host = process.env.DB_HOST
-    const database = process.env.DB_NAME
-    const password = process.env.DB_PSWD
-    const port = process.env.DB_PORT
+    // const user = process.env.DB_USER
+    // const host = process.env.DB_HOST
+    // const database = process.env.DB_NAME
+    // const password = process.env.DB_PSWD
+    // const port = process.env.DB_PORT
 
-    this.pool = new Pool({user, host, database, password, port})
-    return {user, host, database, password, port}
+    // this.pool = new Pool({user, host, database, password, port})
+    // return {user, host, database, password, port}
+    
+    this.pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    })
 }
 
 exports.close = async function() {
